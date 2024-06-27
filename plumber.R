1b5a8a278dd2a8e9bf3562104dc726b549322f4f
gitlexander <astavro123@gmail.com>
2024-06-27 15:34:55 -0400

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -1,12 +1,13 @@
-# plumber.R
-
 library(plumber)
 
 # Define a Plumber router
 r <- plumb()
 
-# Example endpoint
-r$handle("GET", "/", function(req, res) {
-  res$status(200)
-  list(message = "Hello, world!")
+# Create an endpoint for your function
+r$handle("GET", "/sum", function(a, b) {
+  result <- as.numeric(a) + as.numeric(b)
+  list(result = result)
 })
+
+# Run the API
+r$run(host = "0.0.0.0", port = as.numeric(Sys.getenv("PORT", 8000)))
