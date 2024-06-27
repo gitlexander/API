87dc8c3f95d521c126d39bc4bb545550486d7662
gitlexander <astavro123@gmail.com>
2024-06-27 15:19:47 -0400

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -6,17 +6,7 @@ library(plumber)
 r <- plumb()
 
 # Example endpoint
-r$handle("GET", "/hello", function(req, res) {
+r$handle("GET", "/", function(req, res) {
+  res$status(200)
   list(message = "Hello, world!")
 })
-
-# Run the API
-if (Sys.getenv("PORT") != "") {
-  pr <- plumber$new()
-  pr$bind(port = as.numeric(Sys.getenv("PORT")))
-  pr$run()
-} else {
-  pr <- plumber$new()
-  pr$run(port = 8000)
-}
-
